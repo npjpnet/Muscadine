3b4e5366606960def47a7ffda8d4f0307fb92107
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-03-31 21:55:42 +0900

    webhooks

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,7 +1,9 @@
 import * as HelloWorldService from './services/HelloWorldService'
 import * as AccountService from './services/AccountService'
+import * as RequestService from './services/RequestService'
 
 export default {
   HelloWorldService,
-  AccountService
+  AccountService,
+  RequestService
 }
