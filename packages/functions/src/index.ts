ba09de56f91dc7f6f57b40e34895efc04b09aea9
SAIGUSA Tomotada <smmy@nectarition.jp>
2024-03-23 01:38:33 +0900

    デプロイ設定修正

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -2,7 +2,7 @@ import * as accounts from './controllers/accounts'
 import * as helloworld from './controllers/helloworld'
 import * as requests from './controllers/requests'
 
-export default {
+export {
   accounts,
   helloworld,
   requests
